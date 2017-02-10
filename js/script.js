882e0b43bb81891c1c3f303516559f7a81f76a07
Karim-AS <karim.ait_said@devinci.fr>
2017-02-10 14:52:32 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var myObj = this.responseText;
         var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
-        document.getElementById("xoxo").innerHTML = jsonPretty;
+        document.getElementById("demo").innerHTML = jsonPretty;
     }
 };
 xmlhttp.open("GET", "https://api.blockcypher.com/v1/btc/main", true);
