555f7876265133714f0d7b1bf0fc0f6438b02942
Karim-AS <karim.ait_said@devinci.fr>
2017-02-10 15:25:32 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
-function homePageLoading() {
-    url = 'https://api.blockcypher.com/v1/btc/main';
-    elementID = 'https://karim-as.github.io/bitcoin/';
-    fonctionRequeteApi(url, elementID);
-
-    
     var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var myObj = this.responseText;
         var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
         document.getElementById("demo").innerHTML = jsonPretty;
+        
+function homePageLoading() {
+    url = 'https://api.blockcypher.com/v1/btc/main';
+    elementID = 'https://karim-as.github.io/bitcoin/';
+    fonctionRequeteApi(url, elementID);
     }
 };
 xmlhttp.open("GET", "https://api.blockcypher.com/v1/btc/main", true);
