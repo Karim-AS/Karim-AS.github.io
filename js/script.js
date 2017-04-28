a18a9bdc7b2dc7f02eacf704b192cedd5b2f3136
Karim-AS <karim.ait_said@devinci.fr>
2017-04-28 17:35:11 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
-function homePageLoading() {
-    url = 'apiUrl';
-    elementID = 'idDeVotreElementHtml';
-    fonctionRequeteApi(url, elementID);
+function fonctionRequeteApi(url, elementID)
+{
     var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        var myObj = this.responseText;
-        var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
-        document.getElementById("demo").innerHTML = jsonPretty;
-    }
-};
-xmlhttp.open("GET", "https://api.blockcypher.com/v1/btc/main", true);
-xmlhttp.send();}
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var myObj = this.responseText;
+            var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
+            document.getElementById(elementID).innerHTML = jsonPretty;
+        }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+
+function homePageLoading() {
+    var url = 'https://bitcoin.mubiz.com/block/447077/';
+    var elementID = 'resultRequestAPI';
+    fonctionRequeteApi(url, elementID);
+}
