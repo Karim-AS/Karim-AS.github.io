3a0e3818d3d1ea35ba47c29f2719cacc053a65cf
Karim-AS <karim.ait_said@devinci.fr>
2017-04-30 01:21:09 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
+function fonctionRequeteApi(url, elementID)
+{
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var myObj = this.responseText;
+            var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
+            document.getElementById(elementID).innerHTML = jsonPretty;
+        }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
 
-$(document).ready(function() {
-	$.ajax({
-		url : "http://bitcoin.mubiz.com/blocks",
-		dataType : "json",
-		contentType : "application/json; charset=utf-8",
-		type : "GET",
-		timeout:	"5000",
-		async : false,
-
-		success : function(data) {
-			$('#bitcoin_block_number').append(data.blocks);
-		},
-
-		error : function(xhr, status, err) {
-			$('#bitcoin_block_number').append(err+" N/A");
-		}
-	});
-});
+function homePageLoading() {
+    var url = 'https://bitcoin.mubiz.com/block/447077/';
+    var elementID = 'resultRequestAPI';
+    fonctionRequeteApi(url, elementID);
+}
